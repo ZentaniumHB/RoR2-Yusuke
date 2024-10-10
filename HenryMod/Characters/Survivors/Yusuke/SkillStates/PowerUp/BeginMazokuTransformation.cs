0567696b76a07e32faded566a54ac3c7ec48ab66
zentanium <zentaniumhb@gmail.com>
2024-10-10 03:35:41 +0100

    Added basic reduction functionality for Mazoku gauge.

---
+++ HenryMod/Characters/Survivors/Yusuke/SkillStates/PowerUp/BeginMazokuTransformation.cs
@@ -0,0 +1,49 @@
+﻿using EntityStates;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using YusukeMod.Characters.Survivors.Yusuke.Components;
+
+namespace YusukeMod.Characters.Survivors.Yusuke.SkillStates.PowerUp
+{
+    internal class BeginMazokuTransformation : BaseSkillState
+    {
+        public static float baseDuration = 1.25f;
+
+        private float duration = 2f;
+
+        public override void OnEnter()
+        {
+            base.OnEnter();
+            PlayAnimation("Gesture, Override", "ThrowBomb", "ThrowBomb.playbackRate", duration);
+
+        }
+
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+
+            if (isAuthority && fixedAge >= duration)
+            {
+                outer.SetNextStateToMain();
+            }
+        }
+
+        public override void OnExit()
+        {
+            base.OnExit();
+            MazokuComponent maz = characterBody.master.gameObject.GetComponent<MazokuComponent>();
+            if (maz != null)
+            {
+                maz.hasTransformed = true;
+            }
+        }
+
+        public override InterruptPriority GetMinimumInterruptPriority()
+        {
+            return InterruptPriority.Death;
+        }
+
+    }
+}
