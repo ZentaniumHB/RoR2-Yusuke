c056ccd956285850ce407f70ad6acb5907af6fc3
zentanium <zentaniumhb@gmail.com>
2024-10-10 04:27:31 +0100

    Created a icon switch on the baseChargeState depending on what moves have altered versions. Did this a while back and forgot to commit.

--- HenryMod/Modules/BaseContent/BaseStates/BaseChargeSpirit.cs
+++ HenryMod/Modules/BaseContent/BaseStates/BaseChargeSpirit.cs
@@ -1,7 +1,9 @@
 ﻿using EntityStates;
 using RoR2;
+using RoR2.Skills;
 using RoR2.UI;
 using UnityEngine;
+using YusukeMod.Characters.Survivors.Yusuke.Components;
 
 namespace YusukeMod.Survivors.Yusuke.SkillStates
 {
@@ -11,7 +13,8 @@ namespace YusukeMod.Survivors.Yusuke.SkillStates
 
         public float chargeDuration;
         public float baseChargeDuration;
-
+        public SpiritCuffComponent cuffComponent;
+        public const string prefix = YusukeSurvivor.YUSUKE_PREFIX;
 
         // starting value, max value and how fast to increment
         public float chargeValue;
@@ -21,6 +24,7 @@ namespace YusukeMod.Survivors.Yusuke.SkillStates
         public override void OnEnter()
         {
             base.OnEnter();
+            cuffComponent = characterBody.GetComponent<SpiritCuffComponent>();
 
         }
         public override void OnExit()
@@ -34,9 +38,86 @@ namespace YusukeMod.Survivors.Yusuke.SkillStates
             return Mathf.Round(value);
         }
 
-        
+        // reverts the changes of the icons
+        public void RevertIconSwitch(int attackID)
+        {
+            // getting the icons and changing them back 
+            SkillDef primary = skillLocator.primary.skillDef;
+            SkillDef secondary = skillLocator.secondary.skillDef;
+
+            if(attackID == 2)
+            {
+                if (skillLocator.primary.skillNameToken == prefix + "PRIMARY_GUN_NAME")
+                {
+                    primary.icon = YusukeSurvivor.spiritGunIcon;
+                }
+                if (skillLocator.secondary.skillNameToken == prefix + "SECONDARY_GUN_NAME")
+                {
+                    secondary.icon = YusukeSurvivor.spiritGunIcon;
+                }
+            }
+            if (attackID == 3)  // shotgun
+            {
+                if (skillLocator.secondary.skillNameToken == prefix + "SECONDARY_SHOTGUN_NAME")
+                {
+                    secondary.icon = YusukeSurvivor.spiritShotgunIcon;
+                }
+            }
+        }
+
+        // changes the icons when moves are charged, depending if it's maxed or not 
+        public void IconSwitch(bool hasReleased, int attackID)
+        {
+            // getting the icons and changing them accordingly 
+            /*SkillDef primary = skillLocator.primary.skillDef;
+            SkillDef secondary = skillLocator.secondary.skillDef;*/
+
+            if(attackID == 2)
+            {
+                if (skillLocator.primary.skillNameToken == prefix + "PRIMARY_GUN_NAME")
+                {
+                    if (hasReleased)
+                    {
+                        skillLocator.primary.skillDef.icon = YusukeSurvivor.spiritBeamIcon;
+                    }
+                    else
+                    {
+                        skillLocator.primary.skillDef.icon = YusukeSurvivor.spiritGunDoubleIcon;
+                    }
+                }
+                if (skillLocator.secondary.skillNameToken == prefix + "SECONDARY_GUN_NAME")
+                {
+                    if (hasReleased)
+                    {
+                        skillLocator.secondary.skillDef.icon = YusukeSurvivor.spiritBeamIcon;
+                    }
+                    else
+                    {
+                        skillLocator.secondary.skillDef.icon = YusukeSurvivor.spiritGunDoubleIcon;
+                    }
+
+                }
+            }
+            if (attackID == 3) // shotgun
+            {
+                if(skillLocator.secondary.skillNameToken == prefix + "SECONDARY_SHOTGUN_NAME")
+                {
+                    if (hasReleased)
+                    {
+                        skillLocator.secondary.skillDef.icon = YusukeSurvivor.spiritShotGunDoubleIcon;
+                    }
+                    else
+                    {
+                        skillLocator.secondary.skillDef.icon = YusukeSurvivor.spiritShotgunIcon;
+                    }
+                    
+                }
+            }
 
 
+
+
+        }
 
     }
 
