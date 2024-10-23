fae15646e3b53d9f1c05ded009858de362ccb12d
zentanium <zentaniumhb@gmail.com>
2024-10-23 04:04:44 +0100

    Added logic for consecutive grabbing.

--- HenryMod/Characters/Survivors/Yusuke/SkillStates/Grabs/MazBackToBackStrikes.cs
+++ HenryMod/Characters/Survivors/Yusuke/SkillStates/Grabs/MazBackToBackStrikes.cs
@@ -1,13 +1,16 @@
 ﻿using EntityStates;
 using IL.RoR2.Achievements.Bandit2;
 using RoR2;
+using RoR2.Audio;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Networking;
 using YusukeMod.Characters.Survivors.Yusuke.SkillStates.KnockbackStates;
 using YusukeMod.Characters.Survivors.Yusuke.SkillStates.Tracking;
+using YusukeMod.Survivors.Yusuke;
 using YusukeMod.Survivors.Yusuke.SkillStates;
 
 
@@ -19,17 +22,46 @@ namespace YusukeMod.Characters.Survivors.Yusuke.SkillStates.Grabs
 
         private float duration = 0.5f;
         private HurtBox enemyHurtBox = null;
+
         public SingleTracking tracking;
+        private PinnableList pinnableList;
+        private MazokuGrabController mazokuGrabController;
+        private KnockbackController knockbackController;
+
         private bool hasTargetBeenFound;
         private bool isEnemyKilled;
-        private bool isSelectionMade;
+        private bool hasSelectionMade;
+        private float attackStopWatch;
+        private float consecutiveDuration;
+        private float attackInterval = 0.1f;
+        private bool hasIncreaseAttackSpeed;
+
+
+        private OverlapAttack attack;
+        protected DamageType damageType = DamageType.Stun1s;
+        protected float damageCoefficient = 8f;
+        protected float procCoefficient = 1f;
+        protected float pushForce = 350f;
+        protected float radius = 6f;
+        public GameObject hitEffectPrefab = YusukeAssets.swordHitImpactEffect;
+        protected NetworkSoundEventIndex impactSound = YusukeAssets.swordHitSoundEvent.index;
+        protected string hitboxGroupName = "SwordGroup";
+
 
         public override void OnEnter()
         {
             base.OnEnter();
             tracking = gameObject.GetComponent<SingleTracking>();
+            mazokuGrabController = new MazokuGrabController();
+
+            pinnableList = new PinnableList();
+            pinnableList = gameObject.AddComponent<PinnableList>();
+
+            knockbackController = new KnockbackController();
+
             isEnemyKilled = false;
-            isSelectionMade = false;
+            hasSelectionMade = false;
+            characterMotor.Motor.ForceUnground();
         }
 
         private void TeleportToTarget()
@@ -47,8 +79,9 @@ namespace YusukeMod.Characters.Survivors.Yusuke.SkillStates.Grabs
             {
                 // get the enemies model that is marked and teleport to them.
                 enemyHurtBox = tracking.GetTrackingTarget();
-                TeleportToTarget();
+                if(!hasTargetBeenFound) TeleportToTarget();
             }
+
 
             if (!hasTargetBeenFound) 
             {
@@ -58,11 +91,24 @@ namespace YusukeMod.Characters.Survivors.Yusuke.SkillStates.Grabs
             else
             {
                 // if the target is found, then attack the grabbed enemy if they are not killed or a followup selection is made.
-                if(isEnemyKilled || isSelectionMade) ConsecutiveAttack();
+                ConsecutiveAttack();
+
+                if (inputBank.skill1.down)
+                {
+                    mazokuGrabController.EnemyRotation(mazokuGrabController.modelTransform, false);
+                    if (mazokuGrabController.hasRevertedRotation)
+                    {
+                        mazokuGrabController.Remove();
+                        hasSelectionMade = true;
+                    }   
+                }
             }
 
-            if (isAuthority && fixedAge >= duration)
+
+
+            if (isAuthority && fixedAge >= duration && hasSelectionMade || isEnemyKilled)
             {
+                Log.Info("Stop attacking (mazoku)");
                 outer.SetNextStateToMain();
             }
         }
@@ -71,13 +117,85 @@ namespace YusukeMod.Characters.Survivors.Yusuke.SkillStates.Grabs
         {
             // if the character is in the list or not, do different things. 
 
+            characterBody.SetAimTimer(0.5f); 
+            attackStopWatch += GetDeltaTime();
+            consecutiveDuration += GetDeltaTime();
+            Log.Info("consecutive: " + consecutiveDuration);
+
+            if (!hasIncreaseAttackSpeed && consecutiveDuration > 2)
+            {
+                hasIncreaseAttackSpeed = true;
+                attackInterval /= 2;
+            }
+
+            if (enemyHurtBox.healthComponent.alive)
+            {
+                if (attackStopWatch > attackInterval)
+                {
+
+                    if (NetworkServer.active)
+                    {
+                        //Log.Info("Animaton" );
+                        PlayAnimation("Gesture, Override", "ThrowBomb", "ThrowBomb.playbackRate", 0.2f);
+
+
+                    }
+                    EffectManager.SpawnEffect(hitEffectPrefab, new EffectData
+                    {
+                        origin = enemyHurtBox.gameObject.transform.position,
+                        scale = 8f
+                    }, transmit: true);
+
+                    attack = new OverlapAttack
+                    {
+                        damageType = damageType,
+                        attacker = gameObject,
+                        inflictor = gameObject,
+                        teamIndex = GetTeam(),
+                        damage = damageCoefficient * damageStat,
+                        procCoefficient = procCoefficient,
+                        hitEffectPrefab = hitEffectPrefab,
+                        pushAwayForce = pushForce,
+                        hitBoxGroup = FindHitBoxGroup(hitboxGroupName),
+                        isCrit = RollCrit(),
+                        impactSound = impactSound
+                    };
+                    attack.Fire();
+                    attackStopWatch = 0;
+
+                }
+
+
+            }
+
+            if (!enemyHurtBox.healthComponent.alive)
+            {
+                if (enemyHurtBox.healthComponent.health < 1) //using alive doesn't work here
+                {
+                    mazokuGrabController.Remove();
+                    isEnemyKilled = true;
+                }
+                    
+            }
+            else
+            {
+                if (hasSelectionMade)
+                {
+
+                    Log.Info("BARAGE HAS BEEN COMPLETE, REMOVING DIVE COTROLLER");
+                    Log.Info("First deleting dive punch");
+                    
+                }
+            }
+
+
         }
 
         private void FindMatchingHurtbox()
         {
             // grab the enemy collders that are nearby
             Collider[] collisions;
-            collisions = Physics.OverlapSphere(transform.position, 2, LayerIndex.entityPrecise.mask);
+            collisions = Physics.OverlapSphere(transform.position, 50, LayerIndex.entityPrecise.mask);
             List<Collider> colliders = collisions.ToList();
 
             foreach (Collider collider in colliders)
@@ -96,7 +214,24 @@ namespace YusukeMod.Characters.Survivors.Yusuke.SkillStates.Grabs
 
         private void GrabEnemy()
         {
-            
+            mazokuGrabController = enemyHurtBox.healthComponent.body.gameObject.AddComponent<MazokuGrabController>();
+            if (pinnableList)
+            {
+                if (!pinnableList.CheckIfNotPinnable(enemyHurtBox.healthComponent.gameObject.name))
+                {
+                    Log.Info("This character is not in the list, gut punch.");
+                    mazokuGrabController.pivotTransform = FindModelChild("HandR");
+
+                }
+                else
+                {
+                    mazokuGrabController.pivotTransform = FindModelChild("HandR"); // make it pivot to a different bone or object (set it up in the 
+                    Log.Info("This character is  in the list, headbutt");
+
+
+                }
+
+            }
         }
 
         public override void OnExit()
---
+++ HenryMod/Characters/Survivors/Yusuke/SkillStates/Grabs/MazokuGrabController.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using RoR2;
+using EntityStates;
+
+namespace YusukeMod.Characters.Survivors.Yusuke.SkillStates.Grabs
+{
+    internal class MazokuGrabController : MonoBehaviour
+    {
+
+        public Transform pivotTransform;
+        public Vector3 centerOfCollider;
+        public Transform modelTransform;
+
+        private CharacterBody body;
+        private CharacterMotor motor;
+        private CharacterDirection direction;
+        private Rigidbody rigidbody;
+        private RigidbodyMotor rigidMotor;
+        private ModelLocator modelLocator;
+
+        private Quaternion originalRotation;
+        private Collider collider;
+        private SphereCollider sphCollider;
+        private CapsuleCollider capCollider;
+        private Vector3 centerOfMass;
+        private BaseState state;
+
+        private bool setBounds;
+        public bool Pinnable;
+
+        private Vector3 oldMoveVec;
+        private Quaternion oldModelRotation;
+
+        public bool hasStringEnded;
+        public bool hasLanded;
+        public bool hasRevertedRotation;
+        public float changeInY = 0;
+
+        private void Awake()
+        {
+            body = GetComponent<CharacterBody>();
+            motor = GetComponent<CharacterMotor>();
+            direction = GetComponent<CharacterDirection>();
+            rigidMotor = gameObject.GetComponent<RigidbodyMotor>();
+            modelLocator = GetComponent<ModelLocator>();
+            collider = gameObject.GetComponent<Collider>();
+            sphCollider = gameObject.GetComponent<SphereCollider>();
+            capCollider = gameObject.GetComponent<CapsuleCollider>();
+            rigidbody = gameObject.GetComponent<Rigidbody>();
+
+            state = gameObject.GetComponent<BaseState>();
+
+            if (collider)
+            {
+                collider.enabled = false;
+            }
+            if (sphCollider)
+            {
+                sphCollider.enabled = false;
+            }
+            if (capCollider)
+            {
+                capCollider.enabled = false;
+
+            }
+
+            if (rigidMotor)
+            {
+
+                oldMoveVec = rigidMotor.moveVector;
+            }
+
+
+            if (direction) direction.enabled = false;
+
+            if (modelLocator)
+            {
+                if (modelLocator.modelTransform)
+                {
+                    modelTransform = modelLocator.modelTransform;
+                    originalRotation = modelTransform.rotation;
+
+                    if (modelLocator.gameObject.name == "GreaterWispBody(Clone)")
+                    {
+                        modelLocator.dontDetatchFromParent = true;
+                        modelLocator.dontReleaseModelOnDeath = true;
+                    }
+
+                    modelLocator.enabled = false;
+                }
+            }
+
+            hasRevertedRotation = false;
+            setBounds = false;
+            Log.Info("Setup complete");
+
+        }
+
+        private void FixedUpdate()
+        {
+
+            if (pivotTransform.position != Vector3.zero)
+            {
+                if (!setBounds)
+                {
+                    //Log.Info("Setting bounds");
+                    setBounds = true;
+                    EnemyRotation(modelTransform, true);
+
+                }
+
+
+                //Log.Info("Checking motor");
+                if (motor)
+                {
+                    //Log.Info("motor exists");
+                    motor.disableAirControlUntilCollision = true;
+                    motor.velocity = Vector3.zero;
+                    motor.rootMotion = Vector3.zero;
+                    motor.Motor.SetPosition(pivotTransform.position, true);
+
+                }
+
+                //Log.Info("checking rigidMotor");
+                if (rigidMotor)
+                {
+                    //Log.Info("motor exists");
+                    rigidMotor.moveVector = Vector3.zero;
+                    rigidMotor.rootMotion = Vector3.zero;
+                    if (rigidbody)
+                    {
+                        rigidbody.position = pivotTransform.position;
+                        rigidbody.velocity = Vector3.zero;
+                    }
+                }
+
+                if (hasStringEnded)
+                {
+
+                    if (hasRevertedRotation) Remove();
+                }
+
+                if (hasLanded)
+                {
+                    pivotTransform = gameObject.transform;
+                }
+
+                if (modelTransform)
+                {
+                    modelTransform.position = pivotTransform.position;
+
+
+                }
+
+
+            }
+            else
+            {
+                Log.Info(" pivot Doesn't exist, destory");
+                Destroy(this);
+
+            }
+
+
+
+        }
+
+
+        public void EnemyRotation(Transform model, bool rotate)
+        {
+            if (modelTransform)
+            {
+                Log.Info("Rotating character");
+                // ----------Rotating the character
+                oldModelRotation = model.localRotation;
+
+                Vector3 forwardDirection = model.forward;
+                Quaternion worldRotation = model.rotation;
+
+                // convert the world rotation to local rotation (relative to the current forward direction)
+                Quaternion localRotation = Quaternion.Inverse(Quaternion.LookRotation(forwardDirection)) * worldRotation;
+
+                Log.Info("Pinned = " + rotate + " Model local rotation (befire): " + model.localRotation);
+
+                Quaternion alteredRotation = Quaternion.identity;
+                if (rotate) alteredRotation = Quaternion.Euler(0f, -90f, 0f); // rotating so the enemy faces the sky
+                if (!rotate) alteredRotation = Quaternion.Euler(0f, 90f, 0f); // rotating so the enemy faces the sky
+
+                Quaternion newLocalRotation = localRotation * alteredRotation;
+                //from local to world space
+                Quaternion finalWorldRotation = Quaternion.LookRotation(forwardDirection) * newLocalRotation;
+
+                model.localRotation = finalWorldRotation;
+
+                Log.Info("Pinned = " + rotate + " Model local rotation (after): " + model.localRotation);
+
+                if (!rotate)
+                {
+
+                    hasRevertedRotation = true;
+                    
+                }
+
+
+            }
+            else
+            {
+                Log.Info("No model.");
+            }
+        }
+
+        public void Remove()
+        {
+            Log.Info("[backtoback strikes] enabling and destroying");
+            if (modelLocator) modelLocator.enabled = true;
+            if (modelTransform) modelTransform.rotation = originalRotation;
+            if (direction) direction.enabled = true;
+            if (collider) collider.enabled = true;
+            if (sphCollider) sphCollider.enabled = true;
+            if (capCollider) capCollider.enabled = true;
+            if (motor) motor.enabled = true;
+            if (rigidMotor) rigidMotor.moveVector = oldMoveVec;
+
+
+
+            Destroy(this);
+        }
+    }
+}
